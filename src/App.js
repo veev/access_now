2b1cef759e5fb0704476f719c5be837fff201364
Genevieve Hoffman <genny.hoffman@gmail.com>
2019-06-02 00:31:29 +0300

    trying to get buttons working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App() extends Component {
+class App extends Component {
 
   constructor() {
     super()
 
     this.state = {
-      startState = true
+      startState: true,
+      story: "Business"
     }
   }
-  return (
-    <div className="App">
-      <header className="header">
-        <p>
-          Header info goes here.
-        </p>
-      </header>
-    </div>
-  );
+
+  static defaultProps = {
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  handleStoryChange = (newStory) => {
+    if (newStory !== this.state.story) {
+      this.setState({ story: newStory })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+          <h1>
+            AccessNow KeepItOn Shutdown Stories!
+          </h1>
+        </header>
+        <div className="vizArea"></div>
+        <div className="storyArea">
+          <div className="storyNav" onClick={this.handleStoryChange}>
+            <button id="business" className="storyButton">Business</button>
+            <button id="healthcare" className="storyButton">Healthcare</button>
+            <button id="humanitarian" className="storyButton">Humanitarian Aid</button>
+            <button id="education" className="storyButton">Education</button>
+            <button id="psychology" className="storyButton">Psychology</button>
+            <button id="journalism" className="storyButton">Journalism</button>
+            <button id="freedom" className="storyButton">Freedom of Expression</button>
+          </div>
+        </div>
+        <footer className="footer">
+          <p>
+            Footer info goes here.
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
